9a5b98ba522c1ca0a20d16e645193d9a5e5e2bf5
Tyler Hargett <tyler.hargett@mutualmobile.com>
2013-09-28 13:47:08 -0500

    added messages table

--- Keychat.sql
+++ Keychat.sql
@@ -5,3 +5,4 @@ use Keychat
 
 create table Users (userName varchar(256), email varchar(256), password varchar(256), isBlocked bool, isOnline bool, isAdmin bool, loginAttempts int);
 create table Conversations (userOne varchar(256), userTwo varchar(256), convoID int, convoFile varchar(256));
+create table Messages (messageID int, convoID int, userOne varchar(256), userTwo varchar(256), message text);
