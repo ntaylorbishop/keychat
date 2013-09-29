8237379201f19b3a89350fe62253438605f4c2ad
Christopher Kvamme <cpkvamme@gmail.com>
2013-09-29 13:36:24 -0500

    Registration working

--- php/reg.php
+++ php/reg.php
@@ -13,17 +13,23 @@
 		mysql_select_db("Keychat", $con)
 			or die("Unable to connect to the database : " . mysql_error());
 
-		$qry="INSERT INTO Users(userName,password)VALUES('".mysql_escape_string($username)."'
+		$query="INSERT INTO Users(userName, password)VALUES('".mysql_escape_string($username)."'
 		,'".mysql_escape_string($password)."')";
 
 	$result = mysql_query($query);
 
-	if (mysql_num_rows($result) == 0)
-			header('Location: http://Keychat/error.html');
-	else
-			header('Location: http://Keychat/success.html');
 
-	mysql_close($con);
+	$query2 = " select * from Users where userName = '".mysql_escape_string($username)."'";
+
+    $result2 = mysql_query($query2);
+
+    if (mysql_num_rows($result2) == 0)
+   		header('Location: http://Keychat/error.html');
+    else
+   		header('Location: http://Keychat/success.html');
+	
+    mysql_close($con);
+
 
  ?>
 </body></html>
