bd1c3ea3a9c227417838917a0fe8d7ff3a9cbb9e
Christopher Kvamme <cpkvamme@gmail.com>
2013-10-02 11:10:12 -0500

    commented out session for now

--- php/login.php
+++ php/login.php
@@ -7,9 +7,9 @@
 	if (mysql_num_rows($loginresult) == 0)
    		echo "<html><body> Failure logging in </body></html>";
    else
-   		session_start();
+   		/*session_start();
 		$sid = session_id();
-		$_SESSION["userName"] = $username;
+		$_SESSION["userName"] = $username;*/
    		echo "<html><body> Success logging in </body></html>";
 	
    mysql_close($con);
