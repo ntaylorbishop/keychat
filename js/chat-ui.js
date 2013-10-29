1af313f0efaf9499f9cc9f5094a697a4f63ec789
Calvin Owens <jcalvinowens@gmail.com>
2013-10-28 19:52:05 -0500

    Add code to display gotten messages

--- js/chat-ui.js
+++ js/chat-ui.js
@@ -1,4 +1,4 @@
-function show_message_in_window(text)
+function show_sent_message_in_window(text)
 {
 	var d = document.getElementById("messagelog");
 	var p = document.createElement("p");
@@ -7,11 +7,19 @@ function show_message_in_window(text)
 	d.appendChild(p);
 }
 
+function show_got_message_in_window(text)
+{
+	var d = document.getElementById("messagelog");
+	var p = document.createElement("p");
+	p.className = "got-message";
+	p.innerHTML = "Them: " + text;
+	d.appendChild(p);
+}
 
 /* Interface between UI javascript and encryption backend javascript */
 var interface_ops = {
 	"got_message": null,
-	"sent_message": show_message_in_window,
+	"sent_message": show_sent_message_in_window,
 	"init_conversation": null,
 	"end_conversation": null
 };
