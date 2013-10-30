69408c2ee29d206f26385fcbdcf09e978b813196
Calvin Owens <jcalvinowens@gmail.com>
2013-10-29 20:48:44 -0500

    Implement registration

--- php/reg.php
+++ php/reg.php
@@ -1,53 +1,31 @@
-<html><body> 
- <?php
+<?php
 
- 	$c = mysql_connect("localhost", "phpuser", "password");
-	if (!$c) {
-		echo "Database connection failed!\n";
-		die();
-	}
-	//phpinfo();
-  	$username=isset($_POST["user"])?$_POST["user"]:"";
+	$c = mysql_connect("localhost", "phpuser", "password");
+	if (!$c)
+		die("Connection failed");
+	mysql_select_db("keychat");
 
-  	$password=isset($_POST["pwd"])?$_POST["pwd"]:"";
+	if (!isset($_POST['username']) || !isset($_POST['password']))
+		die("YO, YOU FORGOT THE DATA, BITCH");
 
-  	/*connect to DB */
-	//include 'dbcon.php';
+	$username = mysql_real_escape_string($_POST["username"]);
+	$password = mysql_real_escape_string($_POST["password"]);
 
-	/* check to see if user exists */
-	$query1 = " select * from users where username = '".mysql_escape_string($username)."'";
+	$check = mysql_query("select * from users where username = '$username'");
 
-    $check = mysql_query($query1);
+	if (!mysql_num_rows($check)) {
+		$insertion = mysql_query("INSERT INTO users (username, password) VALUES ('$username','$password')");
+		$user_id = mysql_insert_id();
 
-    if (mysql_num_rows($check) == 0){
-		/* make user */
-		$query2 = "INSERT INTO users(username, password)VALUES";
-		$query2 = $query2 . "('".mysql_escape_string($username)."','".mysql_escape_string($password)."')";
-
-		$insertion = mysql_query($query2);
-		$id = mysql_insert_id();
-
-		if (mysql_num_rows($confirm) == 0)
-		   	echo "error registering";	
-			
-		else {
-			echo "You have been succesfully registered";
+		if (!$insertion) {
+			die("DATABASE ERROR, BITCH");
+		} else {
 			session_start();
-			$_SESSION["user_id"] = $loginresult['id'];
-			echo "<html><body> Success logging in </body></html>";
-			$loginquery = "UPDATE users SET isonline='1' WHERE id='$user_id'";
-			mysql_query($loginquery);
-
+			$_SESSION["user_id"] = $user_id;
+			$r = mysql_query("UPDATE users SET isonline=true WHERE id='$user_id'");
+			echo "YO:".$user_id;
 		}
-
+	} else {
+		echo "you already exist";
 	}
-		
-    else {
-    	echo "you already exist";
-    }
-   	
-
-    mysql_close($con);
-
- ?>
-</body></html>
+?>
